856740973c1fa72cccf1fefa14b282c93577582d
Ryad Chair <r.chair@hotmail.fr>
2023-08-31 17:16:36 +0200

    :sparkles: :recycle: feat:/refactor: add inc and dec on area click & refactor inc and dec functions

--- script.js
+++ script.js
@@ -9,29 +9,54 @@ const el_InputDec = document.querySelector('.counter-decrement__input');
 
 const el_ResetBtn = document.querySelector('.reset');
 
+const el_AreaClick = document.querySelector('.click-area');
+
 
 // console.log(el_BtnDec)
 
-
-/* Incrémentation du compteur */
-el_BtnInc.addEventListener('click', () => {
-  // console.log(e)
-  let counterValue = Number(el_Counter.textContent);
-  let incrementValue = Number(el_InputInc.value);
-  let newCounterValue = counterValue + incrementValue;
-  el_Counter.textContent = newCounterValue;
-});
-
-/* Décrémentation du compteur */
-el_BtnDec.addEventListener('click', () => {
+/*** FONCTIONS ***/
+/**
+ * Decrement the counter with the input decrement value
+ */
+function decrementCount() {
   let counterValue = Number(el_Counter.textContent);
   let decrementValue = Number(el_InputDec.value);
   let newCounterValue = counterValue - decrementValue;
   el_Counter.textContent = newCounterValue;
+}
+
+/**
+ * Increment the counter with the input decrement value
+ */
+function incrementCount() {
+  let counterValue = Number(el_Counter.textContent);
+  let incrementValue = Number(el_InputInc.value);
+  let newCounterValue = counterValue + incrementValue;
+  el_Counter.textContent = newCounterValue;
+}
+
+
+/*** COOOOOOODE ***/
+
+/* Incrémentation du compteur */
+el_BtnInc.addEventListener('click', () => {
+  incrementCount();
 });
 
+/* Décrémentation du compteur */
+el_BtnDec.addEventListener('click', () => {
+  decrementCount();
+});
 
 /* Remettre le compteur à 0 avec le bouton "Reset" */
 el_ResetBtn.addEventListener('click', () => {
   el_Counter.textContent = 0;
 })
+
+/* Décrémenter au clique droit sur la zone de cliques */
+el_AreaClick.addEventListener('contextmenu', (e) => {
+  // Annuler l'affichage du menu contextuel
+  e.preventDefault();
+  decrementCount();
+});
+
